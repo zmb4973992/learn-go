4e12dd831a6f8fd950da94f6027c937277e3f828
zmb <19725912@qq.com>
2022-05-05 08:30:15 +0800

    1

--- util/ErrorCode.go
+++ util/ErrorCode.go
@@ -6,14 +6,14 @@ const (
 
 	//用户模块的错误
 	Error_Username_Exist     = 1001
-	Error_Password_incorrect = 1002
+	Error_Password_Incorrect = 1002
 )
 
 var ErrorMessage = map[uint64]string{
 	Success:                  "ok",
 	Error:                    "发生错误",
 	Error_Username_Exist:     "用户名已存在",
-	Error_Password_incorrect: "密码错误",
+	Error_Password_Incorrect: "密码错误",
 }
 
 func GetErrorMessage(code uint64) string {
