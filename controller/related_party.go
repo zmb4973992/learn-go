448e687d7d436e61278c8ae9794e5c87b0b532a7
zmb <19725912@qq.com>
2022-05-30 20:17:18 +0800

    123

--- controller/related_party.go
+++ controller/related_party.go
@@ -3,14 +3,13 @@ package controller
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	"learn-go/serializer"
 	"learn-go/service"
 	"learn-go/util"
 	"learn-go/util/status"
+	"log"
 	"net/http"
 	"strconv"
-	"strings"
 )
 
 func GetRelatedPartyList(c *gin.Context) {
@@ -45,47 +44,40 @@ func UpdateRelatedParty(c *gin.Context) {
 }
 
 func CreateRelatedParty(c *gin.Context) {
-	var paramIn service.RelatedPartyService
-	err := c.ShouldBind(&paramIn)
-	//单文件上传
-	file1, err1 := c.FormFile("file1")
-	b := strings.Join(file1.Filename, "|")
-	if err1 != nil {
+	file, err := c.FormFile("filename")
+	if err != nil {
+		log.Println(err)
 	} else {
-		id := uuid.New().String()
-		file1.Filename = id + file1.Filename
-		err = c.SaveUploadedFile(file1, util.MyUploadConfig.FullPath+file1.Filename)
-		if err != nil {
-			return
-		}
+		fmt.Println(file.Filename)
 	}
 
-	//多文件上传
-	form, err2 := c.MultipartForm()
-	if err2 != nil {
-		fmt.Println(err2)
-		return
-	}
-	files := form.File["files"]
-	for _, file := range files {
-		id := uuid.New().String()
-		file.Filename = id + file.Filename
-		err = c.SaveUploadedFile(file, util.MyUploadConfig.FullPath+file.Filename)
-		if err != nil {
-			return
-		}
-	}
-	if err != nil {
-		c.JSON(http.StatusOK, serializer.ResponseForDetail{
-			Data:    nil,
-			Code:    status.Error,
-			Message: status.GetMessage(status.Error),
-		})
-		return
-	}
-	res := service.CreateRelatedParty(paramIn)
-	c.JSON(http.StatusOK, res)
 }
+
+//多文件上传
+//form, err2 := c.MultipartForm()
+//if err2 != nil {
+//	fmt.Println(err2)
+//	return
+//}
+//files := form.File["files"]
+//for _, file := range files {
+//	id := uuid.New().String()
+//	file.Filename = id + file.Filename
+//	err = c.SaveUploadedFile(file, util.MyUploadConfig.FullPath+file.Filename)
+//	if err != nil {
+//		return
+//	}
+//}
+//if err != nil {
+//	c.JSON(http.StatusOK, serializer.ResponseForDetail{
+//		Data:    nil,
+//		Code:    status.Error,
+//		Message: status.GetMessage(status.Error),
+//	})
+//	return
+//}
+//res := service.CreateRelatedParty(paramIn)
+//c.JSON(http.StatusOK, res)
 
 func DeleteRelatedParty(c *gin.Context) {
 	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
