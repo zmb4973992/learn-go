b8ab46f64d8aa614b06db61b9e4261cdb87a6d83
zmb <19725912@qq.com>
2022-06-05 20:06:43 +0800

    1236

--- main.go
+++ main.go
@@ -26,5 +26,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 }
--- util/status/code_and_message.go
+++ util/status/code_and_message.go
@@ -1,5 +1,26 @@
 package status
 
+// CustomError 自定义错误类型的结构体
+type CustomError struct {
+	ErrorCode    int
+	ErrorMessage string
+}
+
+//需要实现的方法
+func (e CustomError) Error() string {
+	return e.ErrorMessage //这里的message已经存在，就不用再做校验了
+}
+
+// NewCustomError 自定义错误的构造器
+func NewCustomError(errorCode int) CustomError {
+	instance := CustomError{
+		ErrorCode:    errorCode,
+		ErrorMessage: GetMessage(errorCode),
+	}
+	return instance
+}
+
+//自定义错误的code
 const (
 	Success int = 0
 	Error   int = 500
@@ -27,6 +48,7 @@ const (
 	ErrorFailToEncrypt int = 4001
 )
 
+// Message 自定义错误的message
 var Message = map[int]string{
 	Success: "成功",
 	Error:   "错误",
@@ -51,7 +73,7 @@ var Message = map[int]string{
 func GetMessage(code int) string {
 	message, ok := Message[code]
 	if !ok {
-		return "错误代码未定义返回信息，获取错误信息失败，请检查status/code_and_message相关配置"
+		return "由于错误代码未定义返回信息，导致获取错误信息失败，建议检查status/code_and_message相关配置"
 	}
 	return message
 }
